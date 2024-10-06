018da56efeab6a840625e4d68f6bb1948fecb66e
m4entreprise <thomas.m4entreprise@gmail.com>
2024-10-06 04:01:53 +0200

    back to zero

--- test.py
+++
@@ -1,9 +0,0 @@
-import os
-
-path_to_wkhtmltopdf = r"C:\Program Files\wkhtmltopdf\bin\wkhtmltopdf.exe"
-
-# Test pour vérifier si wkhtmltopdf est accessible
-if not os.path.exists(path_to_wkhtmltopdf):
-    print(f"wkhtmltopdf non trouvé à l'emplacement: {path_to_wkhtmltopdf}")
-else:
-    print(f"wkhtmltopdf trouvé à: {path_to_wkhtmltopdf}")
